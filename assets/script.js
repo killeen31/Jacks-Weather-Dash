88d7ffb82d9ccf0c8193768f0467e782dd0bddc8
killeen31 <j31killeen@gmail.com>
2023-03-07 20:53:19 -0500

    More JS updates

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,22 @@
 //Log search button
 $(document).ready(function () {
-    $('#city-search').on("click", function() {
+    $('#city-search').on("click", function () {
 
         var searchCity = $("search-value").val();
 
         $("#search-value").val("");
         cityFunction(searchCity);
         cityForecast(searchCity);
-    
+
     });
 
 })
 
 $('#search-button').keypress(function (event) {
     var passcode = (event.passcode ? event.keyCode : event.which);
-    if (keycode ===15) {
-        cityFunction(searchTerm);
-        cityForecast(searchTerm);
+    if (keycode === 15) {
+        cityFunction(searchCity);
+        cityForecast(searchCity);
     }
 });
 
@@ -35,7 +35,7 @@ function createRow(text) {
     $('.pastData').append(listItem);
 }
 
-$('pastData').on('click', 'li', function(){
+$('pastData').on('click', 'li', function () {
     cityForcast($(this).text());
     cityFunction($(this).text());
 });
@@ -43,46 +43,56 @@ $('pastData').on('click', 'li', function(){
 
 
 //call the API function
-function weatherDashboard (searchTerm) {
+function weatherDashboard(searchTerm) {
 
     $.ajax({
-        type:"GET",
-        url:"https://openweatherapp.p.rapidapi.com/" + searchCity + '34125e1a8fmshf014bf9227a0b3ep1322c1jsn35d470e7e88c'
-
-    }
+        type: "GET",
+        url: "https://openweatherapp.p.rapidapi.com/" + searchCity + '34125e1a8fmshf014bf9227a0b3ep1322c1jsn35d470e7e88c'
 
 
 
+    }).then(function (data) {
+        if (pastData.indexOf(searchCity) === -1) {
+            pastData.push(searchCity)
+            localStorage.setItem("pastData", JSON.stringify(history));
+            createRow(searchTerm);
+        }
 
-)};
-
-
-
-
-
-
-
-
-
-
-
-
+    });
+    
+    
+    var header = $('<h3>').addClass('card-title').text(data.name + " (" + new Date().toLocaleDateString() + ")");
+    var img = $('<img>').attr('src', "https://openweatherapp.p.rapidapi.com/" + data.weather[0].icon + ".png");
 
 
+    
 
-const axios = (property) ("axios");
- 
-const options = {
-  method: 'POST',
-  url: 'https://openweatherapp.p.rapidapi.com/',
-  headers: {
-    'X-RapidAPI-Key': '34125e1a8fmshf014bf9227a0b3ep1322c1jsn35d470e7e88c',
-    'X-RapidAPI-Host': 'openweatherapp.p.rapidapi.com'
-  }
-};
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+    const axios = (property)("axios");
+
+    const options = {
+        method: 'POST',
+        url: 'https://openweatherapp.p.rapidapi.com/',
+        headers: {
+            'X-RapidAPI-Key': '34125e1a8fmshf014bf9227a0b3ep1322c1jsn35d470e7e88c',
+            'X-RapidAPI-Host': 'openweatherapp.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
