15f599f56de56a20c303503dce2e5dd1d5e6fdcf
killeen31 <j31killeen@gmail.com>
2023-05-14 17:52:50 -0400

    updates to script.js adding getWeather function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,35 @@
 var apiKey = "2ebc4e2b6834276fea4d0908ecca1c61"
 var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=cleveland&units=imperial&appid="
 
-var searchBtn = document.getElementById('search-button')
-var searchValue = document.getElementById('search-value')
+const searchBtn = document.getElementById('search-button')
+const searchValue = document.getElementById('search-value')
+const searchHistoryEl = document.getElementById('search-history')
+const searchFormEl = document.getElementById('search-form')
+const searchInputEl = document.getElementById('search-input')
+const currentConditionsEl = document.getElementById('current-conditions')
+const forecastEl = document.getElementById('forecast')
+
+async function getWeather(cityName) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
+    const response = await fetch("2ebc4e2b6834276fea4d0908ecca1c61")
+    const data = await response.json()
+    return data
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const currentTemp = document.getElementById('current-temp')
+const currentWind = document.getElementById('current-wind')
+const currentHumidity = document.getElementById('current-humidity')
 
 function currentCity() {
     var cityName = searchValue.value.trim()
@@ -18,3 +45,21 @@ function currentCity() {
 }
 
 searchBtn.addEventListener("click", currentCity)
+
+
+function fiveDayCast() {
+    var cityName = searchValue.value.trim();
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=YOUR_API_KEY`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Process the retrieved data here
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+searchBtn.addEventListener('click', fiveDayCast)
